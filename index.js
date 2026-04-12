4f387f0571c6ad4a791e09ef5a6693f7ac8d9fe3
nodebug <nodebug@outlook.com>
2026-04-13 01:44:21 +0200

    Updated axios, tough-cookie, and http-cookie-agent dependencies to latest versions (#1)

--- index.js
+++ index.js
@@ -1,70 +1,172 @@
-const { log } = require('@nodebug/logger')
-const axios = require('axios').default
-const { CookieJar } = require('tough-cookie')
-const { HttpCookieAgent, HttpsCookieAgent } = require('http-cookie-agent/http')
+import { CookieJar } from 'tough-cookie'
+import { HttpCookieAgent, HttpsCookieAgent } from 'http-cookie-agent/http'
+import axios from 'axios'
 
+/**
+ * @typedef {import('axios').AxiosRequestConfig} AxiosRequestConfig
+ * @typedef {import('axios').AxiosResponse} AxiosResponse
+ * @typedef {import('axios').AxiosError} AxiosError
+ * @typedef {import('axios').AxiosInstance} AxiosInstance
+ */
+
+/**
+ * Creates an HTTP agent with cookie support
+ * @param {CookieJar} jar - Cookie jar instance
+ * @param {boolean} rejectUnauthorized - Whether to reject unauthorized certificates
+ * @returns {HttpCookieAgent}
+ */
+function createHttpAgent(jar, rejectUnauthorized = true) {
+  return new HttpCookieAgent({
+    cookies: { jar },
+    keepAlive: true,
+    rejectUnauthorized,
+  })
+}
+
+/**
+ * Creates an HTTPS agent with cookie support
+ * @param {CookieJar} jar - Cookie jar instance
+ * @param {boolean} rejectUnauthorized - Whether to reject unauthorized certificates
+ * @returns {HttpsCookieAgent}
+ */
+function createHttpsAgent(jar, rejectUnauthorized = true) {
+  return new HttpsCookieAgent({
+    cookies: { jar },
+    keepAlive: true,
+    rejectUnauthorized,
+  })
+}
+
+/**
+ * Axios wrapper with cookie jar support and retry logic
+ */
 class Axios {
-  constructor(url) {
-    this.url = url
-    this.jar = new CookieJar()
-    const props = {
-      cookies: { jar: this.jar },
-      keepAlive: true,
-      rejectUnauthorized: false, // disable CA checks
+  /**
+   * @param {string} url - Base URL for requests
+   * @param {boolean} rejectUnauthorized - Whether to reject unauthorized TLS certificates (default: true)
+   */
+  constructor(url, rejectUnauthorized = true) {
+    this._url = url
+    this._jar = new CookieJar()
+    this._rejectUnauthorized = rejectUnauthorized
+
+    const httpAgent = createHttpAgent(this._jar, this._rejectUnauthorized)
+    const httpsAgent = createHttpsAgent(this._jar, this._rejectUnauthorized)
+
+    // Set global agents if not already set
+    if (!axios.defaults.httpAgent) {
+      axios.defaults.httpAgent = httpAgent
     }
-    axios.defaults.httpAgent = new HttpCookieAgent(props)
-    axios.defaults.httpsAgent = new HttpsCookieAgent(props)
-    this.connection = axios.create({
-      baseURL: this.url,
+    if (!axios.defaults.httpsAgent) {
+      axios.defaults.httpsAgent = httpsAgent
+    }
+
+    this._connection = axios.default.create({
+      baseURL: this._url,
     })
   }
 
+  /**
+   * Get the base URL
+   * @returns {string}
+   */
   get url() {
     return this._url
   }
 
+  /**
+   * Set the base URL
+   * @param {string} value - New base URL
+   */
   set url(value) {
     this._url = value
+    this._connection.defaults.baseURL = value
   }
 
-  async request(config) {
-    let response
-    const retry = 3
-    log.info(`Running request for the resource ${this.url}${config.url}`)
+  /**
+   * Get the cookie jar instance
+   * @returns {CookieJar}
+   */
+  get jar() {
+    return this._jar
+  }
 
-    for (let i = 0; i <= retry; i++) {
+  /**
+   * Get the axios instance
+   * @returns {AxiosInstance}
+   */
+  get connection() {
+    return this._connection
+  }
+
+  /**
+   * Perform an HTTP request with retry logic
+   * @param {AxiosRequestConfig} config - Request configuration
+   * @param {number} maxRetries - Maximum number of retries (default: 3)
+   * @returns {Promise<AxiosResponse>}
+   */
+  async request(config, maxRetries = 3) {
+    const retryConfig = { ...config }
+    let lastError
+    
+    for (let i = 0; i <= maxRetries; i++) {
       try {
-        // eslint-disable-next-line no-await-in-loop
-        response = await this.connection.request(config)
-        log.info(
-          `Request returned a response '${response.statusText}' with code '${response.status}'`,
-        )
-        break
-      } catch (err) {
-        response = err.response
-        try {
-          log.error(
-            `Request returned an error '${response.stack}' with code '${response.status}'`,
-          )
-          if (i < retry) {
-            log.info('Retrying API request again')
-          }
-        } catch (ex) {
-          log.error(
-            `Request failed. Request config: ${JSON.stringify(config)}\n${
-              err.stack
-            }`,
-          )
-          log.info('Retrying API request again')
-          if (i === retry) {
-            throw err
-          }
+        const response = await this._connection.request(retryConfig)
+        return response
+      } catch (error) {
+        lastError = error
+        // If this is the last retry, throw the error
+        if (i === maxRetries) {
+          throw error
         }
+        // Wait before retrying (exponential backoff)
+        await new Promise(resolve => setTimeout(resolve, Math.pow(2, i) * 1000))
       }
     }
-    log.debug(`Request config:\n${JSON.stringify(config)}`)
-    return response
+    throw lastError
+  }
+
+  /**
+   * Perform a GET request
+   * @param {string} url - Request URL
+   * @param {AxiosRequestConfig} config - Request configuration
+   * @returns {Promise<AxiosResponse>}
+   */
+  async get(url, config = {}) {
+    return this._connection.get(url, config)
+  }
+
+  /**
+   * Perform a POST request
+   * @param {string} url - Request URL
+   * @param {*} data - Request data
+   * @param {AxiosRequestConfig} config - Request configuration
+   * @returns {Promise<AxiosResponse>}
+   */
+  async post(url, data, config = {}) {
+    return this._connection.post(url, data, config)
+  }
+
+  /**
+   * Perform a PUT request
+   * @param {string} url - Request URL
+   * @param {*} data - Request data
+   * @param {AxiosRequestConfig} config - Request configuration
+   * @returns {Promise<AxiosResponse>}
+   */
+  async put(url, data, config = {}) {
+    return this._connection.put(url, data, config)
+  }
+
+  /**
+   * Perform a DELETE request
+   * @param {string} url - Request URL
+   * @param {AxiosRequestConfig} config - Request configuration
+   * @returns {Promise<AxiosResponse>}
+   */
+  async delete(url, config = {}) {
+    return this._connection.delete(url, config)
   }
 }
 
-module.exports = Axios
+export { Axios, CookieJar }
